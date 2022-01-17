afc368806181b63a22e9f2c0e80266702ecc6fba
Moks <gutamax2@mail.ru>
2022-01-17 21:02:30 +0500

    error test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -9,10 +9,10 @@ public class CashbackHackServiceTest {
     @Test
     public void should() {
         CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 1600;
+        int amount = 1000;
 
         int actual = cashbackHackService.remain(amount);
-        int expected = 400;
+        int expected = 0;
 
         assertEquals(expected, actual);
     }
