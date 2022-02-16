cd024f63a0f7b8b6af8ebf0a1f58251aa9261831
Moks <89726455+moks24@users.noreply.github.com>
2022-02-16 08:05:45 +0500

    Update CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -9,10 +9,10 @@ public class CashbackHackServiceTest {
     @Test
     public void shouldOfferToBuyFor100Rubles(){
         CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 900;
+        int amount = 1000;
 
         int actual = cashbackHackService.remain(amount);
-        int expected = 100;
+        int expected = 0;
 
         assertEquals(actual, expected);
     }
